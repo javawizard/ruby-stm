b0a98a8b84f200f4bd842de9fff6f39b104cfd6d
Alexander Boyd <alex@opengroove.org>
2014-10-22 00:00:55 -0600

    Add BroadcastQueue and BroadcastEndpoint

--- lib/stm/datatypes.rb
+++ lib/stm/datatypes.rb
@@ -3,10 +3,12 @@ require_relative 'core'
 require 'ttftree'
 
 module STM
+  class Empty < StandardError
+  end
+  
+  class Full < StandardError
+  end
 end
-
-
-class STM::TValueIterator
 
 
 class STM::TArray
@@ -51,6 +53,79 @@ class STM::TArray
   
   def to_a
     [*self]
+  end
+end
+
+
+class STM::BroadcastQueue
+  def initialize
+    @next_var_container = STM::TVar.new STM::TVar.new(nil)
+    @written = STM::TVar.new 0
+  end
+  
+  def put(value)
+    next_var = STM::TVar.new
+    item = [value, next_var]
+    @next_var_container.get.set item
+    @next_var_container.set next_var
+    @written.set @written.get + 1
+  end
+  
+  def new_endpoint
+    STM::BroadcastEndpoint.new(self, @next_var_container.get, @written.get)
+  end
+  
+  # Internal: ...
+  def written
+    @written.get
+  end
+end
+
+
+class STM::BroadcastEndpoint
+  # Internal: ...
+  def initialize(queue, var, read)
+    @queue = queue
+    @next_var_container = STM::TVar.new var
+    @read = STM::TVar.new read
+  end
+  
+  def get(block=true)
+    if @next_var_container.get.get.nil?
+      if block
+        STM.try_later
+      else
+        raise Empty
+      end
+    else
+      value, next_var = @next_var_container.get.get
+      @next_var_container.set next_var
+      @read.set @read.get + 1
+      value
+    end
+  end
+  
+  def replace(value)
+    @next_var_container.set STM::TVar.new([value, @next_var_container.get])
+    @read.set @read.get - 1
+  end
+  
+  def peek(block=false)
+    value = get(block)
+    replace(value)
+    value
+  end
+  
+  def empty?
+    @next_var_container.get.get.nil?
+  end
+  
+  def duplicate
+    BroadcastEndpoint.new(@queue, @next_var_container.get, @read.get)
+  end
+  
+  def remaining
+    @queue.written - @read.get
   end
 end
 
