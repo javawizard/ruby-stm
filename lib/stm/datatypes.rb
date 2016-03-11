a1eca98e1efb87d3e0ab41e0bfbcb51c97f3cf3b
Alex Boyd <alex@opengroove.org>
2016-03-10 17:23:11 -0700

    Use STM::Helpers in TList and queue/endpoint stuff

--- lib/stm/datatypes.rb
+++ lib/stm/datatypes.rb
@@ -1,56 +1,61 @@
 
 require_relative 'core'
+require_relative 'datatypes'
 require 'ttftree'
 
 module STM
   class Empty < StandardError
   end
-  
+
   class Full < StandardError
   end
 end
 
 
 class STM::TArray
+  include STM::Helpers
+
+  attr_transactional :tree
+
   def initialize
-    @var = STM::TVar.new(TTFTree::Empty.new(TTFTree::Measures::ITEM_COUNT))
+    self.tree = TTFTree::Empty.new(TTFTree::Measures::ITEM_COUNT)
   end
-  
+
   def [](index)
     # TODO: Handle ranges and negative indexes
-    left, right = @var.get.partition { |v| v > index }
+    left, right = tree.partition { |v| v > index }
     right.empty? ? nil : right.first
   end
-  
+
   def []=(index, value)
-    left, right = @var.get.partition { |v| v > index }
-    @var.set left.add_last(value).append(right.without_first)
+    left, right = tree.partition { |v| v > index }
+    tree = left.add_last(value).append(right.without_first)
   end
-  
+
   def delete_at(index)
-    left, right = @var.get.partition { |v| v > index }
+    left, right = tree.partition { |v| v > index }
     return nil if right.empty?
-    @var.set left.append(right.without_first)
+    tree = left.append(right.without_first)
     right.first
   end
-  
+
   def length
-    @var.get.annotation
+    tree.annotation
   end
-  
+
   def insert(index, *values)
-    left, right = @var.get.partition { |v| v > index }
-    left.append(TTFTree.to_tree(@var.get.measure, values)).append(right)
+    left, right = tree.partition { |v| v > index }
+    tree = left.append(TTFTree.to_tree(tree.measure, values)).append(right)
   end
-  
+
   def each
-    tree = @var.get
-    until tree.empty?
-      yield tree.first
-      tree = tree.without_first
+    current_tree = tree
+    until current_tree.empty?
+      yield current_tree.first
+      current_tree = current_tree.without_first
     end
   end
-  
+
   def to_a
     [*self]
   end
@@ -58,74 +63,76 @@ end
 
 
 class STM::BroadcastQueue
+  include STM::Helpers
+
+  attr_transactional :next_var, :written
+
   def initialize
-    @next_var_container = STM::TVar.new STM::TVar.new(nil)
-    @written = STM::TVar.new 0
+    self.next_var = tvar(nil)
+    self.written = 0
   end
-  
+
   def put(value)
-    next_var = STM::TVar.new
-    item = [value, next_var]
-    @next_var_container.get.set item
-    @next_var_container.set next_var
-    @written.set @written.get + 1
+    v = tvar(nil)
+    item = [value, v]
+    self.next_var.set item
+    self.next_var = v
+    self.written += 1
   end
-  
+
   def new_endpoint
-    STM::BroadcastEndpoint.new(self, @next_var_container.get, @written.get)
-  end
-  
-  # Internal: ...
-  def written
-    @written.get
+    STM::BroadcastEndpoint.new(self, next_var, written)
   end
 end
 
 
 class STM::BroadcastEndpoint
-  # Internal: ...
+  include STM::Helpers
+
+  attr_transactional :next_var, :read
+
   def initialize(queue, var, read)
     @queue = queue
-    @next_var_container = STM::TVar.new var
-    @read = STM::TVar.new read
+    self.next_var = var
+    self.read = read
   end
-  
+
   def get(block=true)
-    if @next_var_container.get.get.nil?
+    if next_var.get.nil?
       if block
-        STM.try_later
+        try_later
       else
         raise Empty
       end
     else
-      value, next_var = @next_var_container.get.get
-      @next_var_container.set next_var
-      @read.set @read.get + 1
+      value, v = next_var.get
+      self.next_var = v
+      self.read += 1
       value
     end
   end
-  
+
   def replace(value)
-    @next_var_container.set STM::TVar.new([value, @next_var_container.get])
-    @read.set @read.get - 1
+    self.next_var = tvar([value, self.next_var])
+    self.read -= 1
   end
-  
+
   def peek(block=false)
     value = get(block)
     replace(value)
     value
   end
-  
+
   def empty?
-    @next_var_container.get.get.nil?
+    next_var.get.nil?
   end
-  
+
   def duplicate
-    BroadcastEndpoint.new(@queue, @next_var_container.get, @read.get)
+    BroadcastEndpoint.new(@queue, next_var, read)
   end
-  
+
   def remaining
-    @queue.written - @read.get
+    @queue.written - read
   end
 end
 
